fbe229d294eead57ff5b8eda00878edbebff9280
waheed <elgohary.wahed@gmail.com>
2025-05-15 14:07:37 +0300

    fix

--- src/FirebaseNotificationServiceProvider.php
+++ src/FirebaseNotificationServiceProvider.php
@@ -10,16 +10,23 @@ class FirebaseNotificationServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->publishes([
-            __DIR__.'/../config/firebase.php' => config_path('firebase.php'),
-        ]);
+            __DIR__ . '/config/firebase.php' => config_path('firebase.php'),
+        ], 'firebase-config');
     }
 
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/firebase.php', 'firebase');
+        $this->mergeConfigFrom(
+            __DIR__ . '/config/firebase.php', 'firebase'
+        );
 
-        $this->app->singleton('firebase-notification', function ($app) {
+        $this->app->singleton(FirebaseNotificationService::class, function ($app) {
             return new FirebaseNotificationService();
         });
+
+        // Optional: For backward compatibility
+        $this->app->alias(FirebaseNotificationService::class, 'firebase-notification');
     }
 }
+
+
